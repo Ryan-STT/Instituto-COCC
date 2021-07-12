3ca6c7c989f9f10a987a260043c7de0412c964f3
Ryan-STT <80019514+Ryan-STT@users.noreply.github.com>
2021-07-11 21:12:47 -0300

    Add files via upload

---
+++ demoday.js
@@ -0,0 +1,165 @@
+function saibamais1(){
+    var pontos=document.getElementById("pontosjuh");
+    var maistexto=document.getElementById("saibamaisjuh");
+    var btnSaibaMais=document.getElementById("saibamais1");
+    if(pontos.style.display === "none"){
+        pontos.style.display="inline";
+        maistexto.style.display="none";
+        btnSaibaMais.innerHTML="Saiba Mais";
+    }else{
+        pontos.style.display="none";
+        maistexto.style.display="inline";
+        btnSaibaMais.innerHTML="Fechar"; 
+    }
+}
+/* Olavo */
+function saibamais2(){
+    var pontos=document.getElementById("pontosolavo");
+    var maistexto=document.getElementById("saibamaisolavo");
+    var btnSaibaMais=document.getElementById("saibamais2");
+    if(pontos.style.display === "none"){
+        pontos.style.display="inline";
+        maistexto.style.display="none";
+        btnSaibaMais.innerHTML="Saiba Mais";
+    }else{
+        pontos.style.display="none";
+        maistexto.style.display="inline";
+        btnSaibaMais.innerHTML="Fechar"; 
+    }
+}
+/* Gripp */
+function saibamais3(){
+    var pontos=document.getElementById("pontosgripp");
+    var maistexto=document.getElementById("saibamaisgripp");
+    var btnSaibaMais=document.getElementById("saibamais3");
+    if(pontos.style.display === "none"){
+        pontos.style.display="inline";
+        maistexto.style.display="none";
+        btnSaibaMais.innerHTML="Saiba Mais";
+    }else{
+        pontos.style.display="none";
+        maistexto.style.display="inline";
+        btnSaibaMais.innerHTML="Fechar"; 
+    }
+}
+/* Eevee */
+function saibamais4(){
+    var pontos=document.getElementById("pontoseevee");
+    var maistexto=document.getElementById("saibamaiseevee");
+    var btnSaibaMais=document.getElementById("saibamais4");
+    if(pontos.style.display === "none"){
+        pontos.style.display="inline";
+        maistexto.style.display="none";
+        btnSaibaMais.innerHTML="Saiba Mais";
+    }else{
+        pontos.style.display="none";
+        maistexto.style.display="inline";
+        btnSaibaMais.innerHTML="Fechar"; 
+    }
+}
+/* Murilo */
+function saibamais5(){
+    var pontos=document.getElementById("pontosmurilo");
+    var maistexto=document.getElementById("saibamaismurilo");
+    var btnSaibaMais=document.getElementById("saibamais5");
+    if(pontos.style.display === "none"){
+        pontos.style.display="inline";
+        maistexto.style.display="none";
+        btnSaibaMais.innerHTML="Saiba Mais";
+    }else{
+        pontos.style.display="none";
+        maistexto.style.display="inline";
+        btnSaibaMais.innerHTML="Fechar"; 
+    }
+}
+/* Tata */
+function saibamais6(){
+    var pontos=document.getElementById("pontostata");
+    var maistexto=document.getElementById("saibamaistata");
+    var btnSaibaMais=document.getElementById("saibamais6");
+    if(pontos.style.display === "none"){
+        pontos.style.display="inline";
+        maistexto.style.display="none";
+        btnSaibaMais.innerHTML="Saiba Mais";
+    }else{
+        pontos.style.display="none";
+        maistexto.style.display="inline";
+        btnSaibaMais.innerHTML="Fechar"; 
+    }
+}
+/* Naves */
+function saibamais7(){
+    var pontos=document.getElementById("pontosnaves");
+    var maistexto=document.getElementById("saibamaisnaves");
+    var btnSaibaMais=document.getElementById("saibamais7");
+    if(pontos.style.display === "none"){
+        pontos.style.display="inline";
+        maistexto.style.display="none";
+        btnSaibaMais.innerHTML="Saiba Mais";
+    }else{
+        pontos.style.display="none";
+        maistexto.style.display="inline";
+        btnSaibaMais.innerHTML="Fechar"; 
+    }
+}
+/* Amor da minha vida */
+function saibamais8(){
+    var pontos=document.getElementById("pontosryan");
+    var maistexto=document.getElementById("saibamaisryan");
+    var btnSaibaMais=document.getElementById("saibamais8");
+    if(pontos.style.display === "none"){
+        pontos.style.display="inline";
+        maistexto.style.display="none";
+        btnSaibaMais.innerHTML="Saiba Mais";
+    }else{
+        pontos.style.display="none";
+        maistexto.style.display="inline";
+        btnSaibaMais.innerHTML="Fechar"; 
+    }
+}
+/*----------------------------Mentores---------------------------------------*/
+/* Juliana Bodin */
+function saibamais9(){
+    var pontos=document.getElementById("pontosjuliana");
+    var maistexto=document.getElementById("saibamaisjuliana");
+    var btnSaibaMais=document.getElementById("saibamais9");
+    if(pontos.style.display === "none"){
+        pontos.style.display="inline";
+        maistexto.style.display="none";
+        btnSaibaMais.innerHTML="Saiba Mais";
+    }else{
+        pontos.style.display="none";
+        maistexto.style.display="inline";
+        btnSaibaMais.innerHTML="Fechar"; 
+    }
+}
+/* Guilherme Bodra */
+function saibamais10(){
+    var pontos=document.getElementById("pontosbodra");
+    var maistexto=document.getElementById("saibamaisbodra");
+    var btnSaibaMais=document.getElementById("saibamais10");
+    if(pontos.style.display === "none"){
+        pontos.style.display="inline";
+        maistexto.style.display="none";
+        btnSaibaMais.innerHTML="Saiba Mais";
+    }else{
+        pontos.style.display="none";
+        maistexto.style.display="inline";
+        btnSaibaMais.innerHTML="Fechar"; 
+    }
+}
+/* Wesley Linares */
+function saibamais11(){
+    var pontos=document.getElementById("pontoswesley");
+    var maistexto=document.getElementById("saibamaiswesley");
+    var btnSaibaMais=document.getElementById("saibamais11");
+    if(pontos.style.display === "none"){
+        pontos.style.display="inline";
+        maistexto.style.display="none";
+        btnSaibaMais.innerHTML="Saiba Mais";
+    }else{
+        pontos.style.display="none";
+        maistexto.style.display="inline";
+        btnSaibaMais.innerHTML="Fechar"; 
+    }
+}
